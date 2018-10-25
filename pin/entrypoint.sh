ec1467ed404817dceebdad0613ff1bbdc80613b1
Chance Hudson <jchancehud@gmail.com>
2018-10-25 13:12:52 -0500

    Only pin if CIDS_TO_PIN is supplied

--- pin/entrypoint.sh
+++ pin/entrypoint.sh
@@ -13,20 +13,22 @@ ipfs daemon &
 DAEMON_PID=$(echo $!)
 
 if [ -z $CIDS_TO_PIN ]; then
-  CIDS_TO_PIN="/ipfs/QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH"
+  # echo "received empty CIDS_TO_PIN"
+  # CIDS_TO_PIN="/ipfs/Qme7GCRRizTN5G3XJqcFnfmQtjJ7Bh5YDCqM8Zg2nJSnvN"
+else
+  cids=("$CIDS_TO_PIN")
+
+  # Wait for the daemon to initialize
+  sleep 10
+
+  # Pin all the supplied cids
+  for cid in $cids; do
+    echo "pinning $cid"
+    ipfs pin add "$cid" --progress
+  done
+
+  echo "finished pinning"
 fi
-cids=("$CIDS_TO_PIN")
-
-# Wait for the daemon to initialize
-sleep 10
-
-# Pin all the supplied cids
-for cid in $cids; do
-  echo "pinning $cid"
-  ipfs pin add "$cid" --progress
-done
-
-echo "finished pinning"
 
 while ps ax | grep -v grep | grep $DAEMON_PID > /dev/null; do
   sleep 30
