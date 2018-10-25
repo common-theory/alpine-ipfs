390553bb654948b24761ecf0f09ea3f7a06a5a26
Chance Hudson <jchancehud@gmail.com>
2018-10-25 15:22:32 -0500

    Reorder docker hub tags 🌹 [build versions]

--- build_version_tags.sh
+++ build_version_tags.sh
@@ -20,7 +20,7 @@ build_push_version ()
 
 # To build versions earlier than v0.4.14 the base image needs
 # to be switched to golang:1.10.4-alpine3.8
-VERSIONS=("v0.4.17 v0.4.16 v0.4.15 v0.4.14")
+VERSIONS=("v0.4.14 v0.4.15 v0.4.16 v0.4.17")
 
 # Build and push each tag
 for VERSION in $VERSIONS; do
